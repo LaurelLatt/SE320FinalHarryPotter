e6e42a49a8c1eee39a98585746f56d70489b94c7
Laurel Latt <latt@chapman.edu>
2025-05-14 16:49:30 -0700

    refactored some classes and added code to program.cs

--- SE320FinalHarryPotter/Program.cs
+++ SE320FinalHarryPotter/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        User user = new User();
+        
+        int userID = 0;
+        while (userID == 0)
+        {
+            string LoginMenu = @" 
+        Welcome! 
+        1. Login
+        2. Create Account         
+        Select option: ";
+            Console.Write(LoginMenu);
+            string userInput = Console.ReadLine();
+            Console.WriteLine();
+            switch (userInput)
+            {
+                case "1":
+                    Console.Write("Username: ");
+                    string username = Console.ReadLine();
+                    Console.Write("Password: ");
+                    string password = Console.ReadLine();
+                    userID = user.Login(username, password);
+                    break;
+                case "2":
+                    Console.Write("Username: ");
+                    username = Console.ReadLine();
+                    bool uniqueUsername = user.IsUniqueUsername(username);
+                    while (!uniqueUsername)
+                    {
+                        Console.WriteLine("Username already exists!");
+                        Console.Write("Username: ");
+                        username = Console.ReadLine();
+                        uniqueUsername = user.IsUniqueUsername(username);
+                    }
+                    Console.Write("Password: ");
+                    password = Console.ReadLine();
+                    user.CreateAccount(username, password);
+                    userID = user.Login(username, password);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Try again.");
+                    break;
+            }
+            
+        }
+        
     }
+    
 }
